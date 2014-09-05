5abb0053fa0f8e7a8c083a2031b8d472986d664c
Rasheed Amir <rasheed.amir@live.com>
2014-09-05 11:41:19 +0200

    Update Gruntfile.js

--- app/templates/Gruntfile.js
+++ app/templates/Gruntfile.js
@@ -1,4 +1,9 @@
 // Generated on <%= (new Date).toISOString().split('T')[0] %> using <%= pkg.name %> <%= pkg.version %>
+
+// Strict Mode is a new feature in ECMAScript 5 that allows you to place a program, or a function, 
+// in a "strict" operating context. This strict context prevents certain actions from being taken 
+// and throws more exceptions.
+// Read more here: http://ejohn.org/blog/ecmascript-5-strict-mode-json-and-more/
 'use strict';
 
 // # Globbing
